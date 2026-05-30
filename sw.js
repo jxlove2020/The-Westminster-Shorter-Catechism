d3d33435f84630a94946ef085f07bc5ca0aafebe
Joseph <60208201+jxlove2020@users.noreply.github.com>
2026-05-30 19:59:48 +0900

    버그 수정

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'catechism-shell-v4';
+const CACHE_NAME = 'catechism-shell-v5';
 const NETWORK_FIRST = ['./data.js'];
 
 const CORE_ASSETS = [
@@ -15,7 +15,10 @@ const CORE_ASSETS = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS)).then(() => self.skipWaiting()),
+    caches
+      .open(CACHE_NAME)
+      .then(cache => cache.addAll(CORE_ASSETS))
+      .then(() => self.skipWaiting()),
   );
 });
 
